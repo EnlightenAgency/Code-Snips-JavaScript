8d920a93bb2c9724bd656ae8ab7ebae26080ef91
Mike Behnke <mike.behnke@perficient.com>
2016-04-09 12:12:46 -0400

    fix type (decodeURL should be decodeURI)

--- browserStorage.js
+++ browserStorage.js
@@ -78,7 +78,7 @@ var browserStorage = (function() {
 		for (var i = 0; i < ca.length; i++) {
 			c = ca[i];
 			while (c.charAt(0) === ' ') { c = c.substring(1, c.length); }
-			if (c.indexOf(nameEQ) === 0) { return decodeURL(c.substring(nameEQ.length, c.length)); }
+			if (c.indexOf(nameEQ) === 0) { return decodeURI(c.substring(nameEQ.length, c.length)); }
 		}
 
 		return null;
