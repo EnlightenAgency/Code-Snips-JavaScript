de12c2f59d73d53a05d5124a0b5362f855a5ef8e
Mike Behnke <mike.behnke@perficient.com>
2016-04-09 11:12:46 -0400

    update socialSharing module to follow the revealing module pattern, document functions

--- socialSharing.js
+++ socialSharing.js
@@ -5,79 +5,126 @@
 // Sample sharing links reference: http://www.sharelinkgenerator.com/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-;(function($, undefined) {
-	window.socialSharing = {
-		openWindow: function(url, w, h) {  // w/h are optional
-			w = w || 580;
-			h = h || 400;
-			window.open(url, 'shareDialog', 'toolbar=0,status=0,width=' + w + ',height=' + h);
-			return false;
-		},
-		createFBFeedDialogLink: function(FBVars) {
-			// Reference for the Feed Dialog:
-			// http://developers.facebook.com/docs/reference/dialogs/feed/
-			var url = 'http://www.facebook.com/dialog/feed?app_id=' + FBVars.fbAppId + 
-					'&link=' + FBVars.fbShareUrl + 
-					(FBVars.fbSharePicPath !== '' ? '&picture=' + FBVars.fbSharePicPath : '') + 
-					(FBVars.nameLine !== '' ? '&name=' + encodeURIComponent(FBVars.nameLine) : '') + 
-					(FBVars.capt !== '' ? '&caption=' + encodeURIComponent(FBVars.capt) : '') + 
-					(FBVars.desc !== '' ? '&description=' + encodeURIComponent(FBVars.desc) : '') + 
-					'&redirect_uri=' + FBVars.baseURL + 'PopupClose.html' +
-					'&display=popup';
+window.socialSharing = (function($) {
 
-			return url;
-		},
-		createFBSharerLink: function (shareURL) {
-			// Reference for the Facebook Sharer Share:
-			// 
-			var url = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(shareURL);
-
-			return url;
-		},
-		createTwitterShareLink: function(shareText) {
-			// Reference for the Twitter Share:
-			// 
-			var url = 'https://twitter.com/home?status=' + encodeURIComponent(shareText);
-
-			return url;
-		},
-		createLinkedInShareLink: function(LIVars) {
-			// Reference for the LinkedIn Share:
-			// 
-			var url = 'https://www.linkedin.com/shareArticle?mini=true&' + 
-					'url=' + encodeURIComponent(LIVars.shareUrl) + 
-					'&title=' + encodeURIComponent(LIVars.shareTitle) + 
-					(LIVars.shareSummary !== '' ? '&summary=' + encodeURIComponent(LIVars.shareSummary) : '') + 
-					(LIVars.shareSource !== '' ? '&source=' + encodeURIComponent(LIVars.shareSource) : '');
-
-			return url;
-		},
-		createGPlusShareLink: function(shareURL) {
-			// Reference for the Google+ share
-			//
-
-			var url = 'https://plus.google.com/share?url=' + encodeURIComponent(shareUrl);  
-
-			return url;
-		},
-		createPinterestPinLink: function(PinVars) {
-			// Reference for the Pinterest Pin share
-			//
-
-			var url = 'https://pinterest.com/pin/create/button/?url=' + encodeURIComponent(PinVars.shareUrl) + 
-			'&media=' + encodeURIComponent(PinVars.shareMedia) + 
-			(PinVars.shareDescription !== '' ? '&description=' + encodeURIComponent(PinVars.shareDescription) : '');
-
-			return url;
-		}
+	return {
+		openWindow: openWindow,
+		createFBFeedDialogLink: createFBFeedDialogLink,
+		createFBSharerLink: createFBSharerLink,
+		createTwitterShareLink: createTwitterShareLink,
+		createLinkedInShareLink: createLinkedInShareLink,
+		createGPlusShareLink: createGPlusShareLink,
+		createPinterestPinLink: createPinterestPinLink
 	};
-})(jQuery);
 
+	/**
+	 * openWindow - open a popup window to load the sharing link in
+	 * @param  {string} url - URL to open
+	 * @param  {number} width - width of the window (optional)
+	 * @param  {number} height - height of the window (optional)
+	 * @param  {string} winName - window name (optional)
+	 * @return {WindowObjectReference} - Reference to the newly created window or null if failed
+	 */
+	function openWindow(url, width, height, winName) {
+		width = width || 580;
+		height = height || 400;
+		winName = winName || 'shareDialog';
 
-/* Facebook Share requires a Facebook Application ID and uses an FBVars object that can be created from server code or JavaScript */
-/* FBVars oject looks like:
+		return window.open(url, winName, 'toolbar=0,status=0,width=' + width + ',height=' + height);
+	}
 
-var FBVars = {
+	/**
+	 * createFBFeedDialogLink - create a URL for a custom FB feed dialog
+	 * @param  {object} fbVars - custom object containing the Facebook vars to fill in this link
+	 * @return {string} returns URL string
+	 *
+	 * Notes: 
+	 * 		Reference for the Feed Dialog:
+	 * 		http://developers.facebook.com/docs/reference/dialogs/feed/
+	 */
+	function createFBFeedDialogLink(fbVars) {
+		var url = 'http://www.facebook.com/dialog/feed?app_id=' + fbVars.fbAppId + 
+				'&link=' + fbVars.fbShareUrl + 
+				(fbVars.fbSharePicPath !== '' ? '&picture=' + fbVars.fbSharePicPath : '') + 
+				(fbVars.nameLine !== '' ? '&name=' + encodeURIComponent(fbVars.nameLine) : '') + 
+				(fbVars.capt !== '' ? '&caption=' + encodeURIComponent(fbVars.capt) : '') + 
+				(fbVars.desc !== '' ? '&description=' + encodeURIComponent(fbVars.desc) : '') + 
+				'&redirect_uri=' + fbVars.baseURL + 'PopupClose.html' +
+				'&display=popup';
+
+		return url;
+	}
+
+	/**
+	 * createFBSharerLink - create a URL for FB sharer dialog
+	 * @param  {string} shareURL - url to share
+	 * @return {string} returns URL string
+	 */
+	function createFBSharerLink(shareURL) {
+		var url = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(shareURL);
+
+		return url;
+	}
+
+	/**
+	 * createTwitterShareLink - create a URL for Twitter share dialog
+	 * @param  {string} shareText - text to share on Twitter
+	 * @return {string} returns URL string
+	 */
+	function createTwitterShareLink(shareText) {
+		var url = 'https://twitter.com/home?status=' + encodeURIComponent(shareText);
+
+		return url;
+	}
+
+	/**
+	 * createLinkedInShareLink - create a URL for LinkedIn share dialog
+	 * @param  {object} LIVars - custom object containing the LinkedIn vars to fill in this link
+	 * @return {string} returns URL string
+	 */
+	function createLinkedInShareLink(LIVars) {
+		var url = 'https://www.linkedin.com/shareArticle?mini=true&' + 
+				'url=' + encodeURIComponent(LIVars.shareUrl) + 
+				'&title=' + encodeURIComponent(LIVars.shareTitle) + 
+				(LIVars.shareSummary !== '' ? '&summary=' + encodeURIComponent(LIVars.shareSummary) : '') + 
+				(LIVars.shareSource !== '' ? '&source=' + encodeURIComponent(LIVars.shareSource) : '');
+
+		return url;
+	}
+
+	/**
+	 * createGPlusShareLink - create a URL for Google Plus share dialog
+	 * @param  {string} shareURL - url to share
+	 * @return {string} returns URL string
+	 */
+	function createGPlusShareLink(shareURL) {
+		var url = 'https://plus.google.com/share?url=' + encodeURIComponent(shareUrl);  
+
+		return url;
+	}
+
+	/**
+	 * createPinterestPinLink - create a URL for Pinterest Pin Dialog
+	 * @param  {object} pinVars - custom object containing the Pinterest vars to fill in this link
+	 * @return {string} returns URL string
+	 */
+	function createPinterestPinLink(pinVars) {
+		var url = 'https://pinterest.com/pin/create/button/?url=' + encodeURIComponent(pinVars.shareUrl) + 
+		'&media=' + encodeURIComponent(pinVars.shareMedia) + 
+		(pinVars.shareDescription !== '' ? '&description=' + encodeURIComponent(pinVars.shareDescription) : '');
+
+		return url;
+	}
+}(jQuery));
+
+//////////////////////////////////////////////////////////////////////
+// Additional information for the Social Sharing module
+//////////////////////////////////////////////////////////////////////
+
+/* Facebook Share requires a Facebook Application ID and uses an fbVars object that can be created from server code or JavaScript */
+/* fbVars oject looks like:
+
+var fbVars = {
 	fbAppId: '',  // Application ID
 	fbShareUrl:'',  // URL to Share
 	fbSharePicPath:'', // Picture URL if you want to customize the image
@@ -114,10 +161,10 @@ var LIVars = {
 }
 */
 
-/* Pinterest Pin Share uses an PinVars object that can be created from server code or JavaScript */
-/* PinVars oject looks like:
+/* Pinterest Pin Share uses an pinVars object that can be created from server code or JavaScript */
+/* pinVars oject looks like:
 
-var PinVars = {
+var pinVars = {
 	shareUrl: '', // image URL
 	shareMedia: '', // image source
 	shareDescription: '' // share description is optional
